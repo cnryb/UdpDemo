55c7d412b3fb5e3c39aa7bfce71c3611be248d49
任跃兵 <cnryb@live.cn>
2019-06-14 11:04:04 +0800

    test

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Client");
+            int port = 443;
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
+            while (true)
+            {
+                var msg = Console.ReadLine();
+                byte[] bs = Encoding.ASCII.GetBytes(msg);
+                await udpClient.SendAsync(bs, bs.Length);
+                UdpReceiveResult res = await udpClient.ReceiveAsync();
+                msg = Encoding.ASCII.GetString(res.Buffer);
+                Console.WriteLine(msg);
+            }
+
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Server");
+            int port = 443;
+
+            UdpClient udpClient = new UdpClient(new IPEndPoint(IPAddress.Any, port));
+            while (true)
+            {
+                UdpReceiveResult res = await udpClient.ReceiveAsync();
+                var msg = Encoding.ASCII.GetString(res.Buffer);
+                Console.WriteLine(msg);
+                await udpClient.SendAsync(res.Buffer, res.Buffer.Length, res.RemoteEndPoint);
+            }
+
+        }
+    }
+}
