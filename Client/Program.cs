9c86f1a052e6435d025947cb40228fa79e0b3c9a
任跃兵 <cnryb@live.cn>
2019-06-14 11:34:15 +0800

    t

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Client
@@ -12,22 +13,43 @@ namespace Client
         {
             Console.WriteLine("Client");
             int port = 443;
-            string ip = "127.0.0.1";
+            string ip = "52.175.49.29";
+            //ip = "127.0.0.1";
             var remote = new IPEndPoint(IPAddress.Parse(ip), port);
 
             UdpClient udpClient = new UdpClient();
             //udpClient.Connect(remote);
-            while (true)
-            {
-                var msg = Console.ReadLine();
-                byte[] bs = Encoding.ASCII.GetBytes(msg);
-                udpClient.Send(bs, bs.Length, remote);
-                //await udpClient.SendAsync(bs, bs.Length);
-                UdpReceiveResult res = await udpClient.ReceiveAsync();
-                msg = Encoding.ASCII.GetString(res.Buffer);
-                Console.WriteLine(msg);
-            }
 
+            byte[] bs = Encoding.ASCII.GetBytes("hi");
+            udpClient.Send(bs, bs.Length, remote);
+
+
+            ThreadPool.QueueUserWorkItem(async q =>
+            {
+                while (true)
+                {
+                    UdpReceiveResult res = await udpClient.ReceiveAsync();
+                    var bbs = res.Buffer;
+                    //var bs = udpClient.Receive(ref remote);
+                    var msg = Encoding.ASCII.GetString(bbs);
+                    Console.WriteLine(msg);
+                }
+            });
+            //while (true)
+            //{
+            //    var msg = Console.ReadLine();
+            //    bs = Encoding.ASCII.GetBytes(msg);
+            //    udpClient.Send(bs, bs.Length, remote);
+            //    //await udpClient.SendAsync(bs, bs.Length);
+            //}
+
+            for (int i = 0; i < 10000; i++)
+            {
+                //var msg = Console.ReadLine();
+                bs = Encoding.ASCII.GetBytes(i.ToString());
+                udpClient.Send(bs, bs.Length, remote);
+                Thread.Sleep(50);
+            }
         }
     }
 }
