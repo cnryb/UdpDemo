08e7dce51d09c62ed7f79659f8fc64baca842cd7
任跃兵 <cnryb@live.cn>
2019-06-14 10:47:32 +0800

    添加项目文件。

---
+++ UdpDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
